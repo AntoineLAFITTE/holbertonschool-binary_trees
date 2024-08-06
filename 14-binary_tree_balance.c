3c7707b1fb8b08f1d9eb9e57d5d2c7143aed2807
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-08-06 10:59:19 +0200

    fixed betty mistake

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -33,6 +33,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int leftHeight, rightHeight;
+
 	if (tree == NULL)
 		return (0);
 	leftHeight = binary_tree_height(tree->left);
